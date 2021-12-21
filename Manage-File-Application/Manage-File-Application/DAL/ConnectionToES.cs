e31e1c52f7f05c5771d0481c5c6bf57299111b67
Nam KuRo <76508715+vannam272008@users.noreply.github.com>
2021-12-21 10:44:34 +0700

    Feature/setup environment (#2)
    * Update Readme.md and set up environment
    
    * Update Readme.md and set up environment

---
+++ Manage-File-Application/Manage-File-Application/DAL/ConnectionToES.cs
@@ -0,0 +1,23 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Example connection to elastic search
+namespace Manage_File_Application.DAL
+{
+    class ConnectionToES
+    {
+        public static ElasticClient EsClient()
+        {
+            ConnectionSettings connectionSettings;
+            ElasticClient elasticClient;
+            connectionSettings = new ConnectionSettings(new Uri("http://localhost:9200/"));
+            elasticClient = new ElasticClient(connectionSettings);
+            return elasticClient;
+        }
+    }
+}
